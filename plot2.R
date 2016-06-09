1dd490efa497850920272e9d243d54d52d38cc52
OliverCSY <shanyun.chu@gmail.com>
2016-06-09 23:27:36 +0100

    Delete plot2.R

--- plot2.R
+++
@@ -1,8 +0,0 @@
-household <- read.csv('household_power_consumption.txt',sep=';',header = TRUE, colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'),na.strings = '?')
-household_subset <- household[which(household$Date=='1/2/2007' | household$Date=='2/2/2007'),]
-
-dateandtime <- paste(household_subset$Date,household_subset$Time)
-date_time <- strptime(dateandtime,'%e/%m/%Y %H:%M:%S')
-png('plot2.png',width=480,height=480)
-plot(date_time,household_subset$Global_active_power,type = 'l',xlab='',ylab='Global Active Power (kilowatts)')
-dev.off()
